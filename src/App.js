8f5f7e218c8c41bcb2d14efaed42bdb95ac53f82
Will Taylor <willtaylor116@gmail.com>
2022-03-22 07:15:02 -0500

    added bootstrap5 and responsive navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './components/Navbar.js';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <div className="container-fluid">
+          <div className='row'>
+            <div className='col'>
+              <button type="button" class="btn btn-primary">Primary</button>
+            </div>
+          </div>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
